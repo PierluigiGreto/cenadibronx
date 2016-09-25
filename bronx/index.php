591c20fa7f77014b91a27251fa67b6d917133775
Pierluigi Greto <gretopierluigi@gmail.com>
2016-09-25 14:11:34 +0000

    sistemata galleria per altervista

--- bronx/index.php
+++ bronx/index.php
@@ -1,6 +1,7 @@
 <?php 
-$directory="./"; 
+$directory=""; 
 foreach (glob($directory.'*') as $file) { 
-echo "File: <a href=\"".$directory.$file."\">".$file."</a><br />Ultima modifica: ".  date( "F d Y h:i:s", getlastmod() ). "<br /> Dimensione: ".filesize($directory.$file)."<br /><br />"; 
+echo $file;
+echo "\n";
 } 
 ?>
